c47ef3b7a68855a53c31e0e2bc9ba38ba6501bda
bokamatyas <boka.matyas@students.jedlik.eu>
2022-12-26 00:35:09 +0100

    janken template, csak hogy fent legyen implementálás előtt

---
+++ minigames/Janken/janken.js
@@ -0,0 +1,78 @@
+const field = document.querySelector(".field");
+const result = document.querySelector(".result");
+const rockButton = document.querySelector("#rock-button");
+const paperButton = document.querySelector("#paper-button");
+const scissorsButton = document.querySelector("#scissors-button");
+
+let hands = [0, "rock", "paper", "scissors"]; //1 -> rock ; 2 -> paper; 3 -> scissors
+let winConditions = ["rockscissors", "paperrock", "scissorspaper"];
+
+
+//methods
+
+
+function botPlays(playerPick) {
+  field.innerHTML = "";
+  result.innerHTML = "";
+  let botPick = Math.ceil(Math.random() * 3);
+  let pick = hands[botPick];
+
+  console.log("----");
+  console.log(playerPick);
+  console.log(pick);
+  console.log("----");
+
+  let image = document.createElement("img");
+  image.src = botPick+".jpg";
+  field.append(image);
+  if(playerPick != pick){
+    checkResults(playerPick, pick);
+  }
+  else{
+    result.innerHTML = "draw";
+    rockButton.disabled = true;
+    paperButton.disabled = true;
+    scissorsButton.disabled = true;
+    continueGame();
+  }
+}
+
+function checkResults(PP, BP){
+  console.log(PP);
+  console.log(BP);
+  if (winConditions.includes(PP+BP) == true){
+    result.innerHTML = "player wins";
+    rockButton.disabled = true;
+    paperButton.disabled = true;
+    scissorsButton.disabled = true;
+  }
+  if(winConditions.includes(PP+BP) == false){
+    result.innerHTML = "bot wins";
+    rockButton.disabled = true;
+    paperButton.disabled = true;
+    scissorsButton.disabled = true;
+  }
+}
+
+function continueGame(){
+  rockButton.disabled = false;
+  paperButton.disabled = false;
+  scissorsButton.disabled = false;
+}
+
+function playRock(){
+  botPlays("rock");
+}
+function playPaper(){
+  botPlays("paper");
+}
+function playScissors(){
+  botPlays("scissors");
+}
+
+// main
+
+rockButton.addEventListener("click", playRock,);
+paperButton.addEventListener("click", playPaper);
+scissorsButton.addEventListener("click", playScissors);
+
